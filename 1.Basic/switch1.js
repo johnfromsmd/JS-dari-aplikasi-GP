86efe501cac8b82ce8e7ae962bfdb66306898515
John From Smd <johnfromsmd@gmail.com>
2020-02-26 20:01:24 +0700

    Tambah file pengulangan switch casing

---
+++ 1.Basic/switch1.js
@@ -0,0 +1,14 @@
+var a = 20;
+var b = "20 equals ";
+function switchc(){
+        document.write(b + "<br>");
+    if(a==10){
+        document.write("a is equal to 10");
+    } else if(a==15){
+        document.write("a is equal to 15");
+    } else if(a==20){
+        document.write("a is equal to 20");
+    } else {
+        document.write("a is not equal 10, 15, or 20");
+    }
+}
---
+++ 1.Basic/switch2.js
@@ -0,0 +1,8 @@
+var grade="B";
+var result;
+function switchca(){
+    switch(grade){
+        case 'A':
+        result = "A grade";
+    break;
+}
