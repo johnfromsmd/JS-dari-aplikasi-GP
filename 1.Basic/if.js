b8d8b25079aafe74254cef77c223928aed123c15
John From Smd <johnfromsmd@gmail.com>
2020-02-26 17:17:14 +0700

    Menambahkan pengulang if, if else, if else if

---
+++ 1.Basic/if.js
@@ -0,0 +1,8 @@
+var a = 20;
+var b = 10;
+function msg(){
+    if(a>b){
+        document.write("Jika " + a +">" + b + "<br>");
+        document.write("Value of a is greater than 10");
+    }
+}
---
+++ 1.Basic/ifelse.js
@@ -0,0 +1,10 @@
+var a = 20;
+var b = "20 % 2 =0 ";
+function ifelse(){
+        document.write(b + "<br>");
+    if(a%2==0){
+        document.write("a is even number");
+    } else {
+        document.write("a is odd number");
+    }
+}
