389a9651669da16beafbf837a5240577d42b34c1
John From Smd <johnfromsmd@gmail.com>
2020-02-27 18:43:51 +0700

    Edit JS array file

--- 1.Basic/arrayconstructor.js
+++ 1.Basic/arrayconstructor.js
@@ -1,3 +1,5 @@
 function jsarrconstructor(){
-    
+    var emp=new Array("Dohn", "John", "Rene");
+    for (i=0; i<emp.length; i++)
+    document.write(emp[i] + "<br/>");
 }
--- 1.Basic/arraydirectly.js
+++ 1.Basic/arraydirectly.js
@@ -1,3 +1,10 @@
 function jsarrdirectly(){
+    var i;
+    var emp = new Array();
+    emp[0] = "Dohn";
+    emp[1] = "John";
+    emp[2] = "Rene";
     
+    for (i=0; i<emp.length; i++)
+    document.write(emp[i] + "<br/>");
 }
