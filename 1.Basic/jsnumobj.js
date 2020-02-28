352c8e54af3260b4928f112349057d45a7dce24c
John From Smd <johnfromsmd@gmail.com>
2020-02-28 17:45:06 +0700

    Tambah File JS Number Objects

---
+++ 1.Basic/jsnumobj.js
@@ -0,0 +1,10 @@
+function msg(){
+    var x=102;
+    var y=102.7;
+    var z=13e4;
+    var n=new Number(16);
+    document.write(x + "<br>");
+    document.write(y + "<br>");
+    document.write(z + "<br>");
+    document.write(n + "<br>");
+}
