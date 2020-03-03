e64d625c8fc01197ca99b222eedc3804bb121a48
John From Smd <johnfromsmd@gmail.com>
2020-03-03 12:49:52 +0700

    JS Navigator Object

---
+++ 1.Basic/jsscreenobject.js
@@ -0,0 +1,31 @@
+function cn(){
+    document.writeln("<br/>navigator.appCodeName:" + navigator.appCodeName);
+}
+
+function an(){
+    document.writeln("<br/>navigator.appName:" + navigator.appName);
+}
+
+function av(){
+    document.writeln("<br/>navigator.appVersion:" + navigator.appVersion);
+}
+
+function ce(){
+    document.writeln("<br/>navigator.cookieEnabled:" + navigator.cookieEnabled);
+}
+
+function lang(){
+    document.writeln("<br/>navigator.language:" + navigator.language);
+}
+
+function ua(){
+    document.writeln("<br/>navigator.userAgent:" + navigator.userAgent);
+}
+
+function plat(){
+    document.writeln("<br/>navigator.platform:" + navigator.platform);
+}
+
+function ol(){
+    document.writeln("<br/>navigator.onLine:" + navigator.onLine);
+}
