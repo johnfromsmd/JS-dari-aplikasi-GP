ac2e0716eb148c08fa2df6bb49ea7ade2d46711c
John From Smd <johnfromsmd@gmail.com>
2020-03-21 13:27:03 +0700

    Tambah Object literal

--- 2.Advanced/Javascript Object/jsobject.js
+++ 2.Advanced/Javascript Object/jsobject.js
@@ -1,5 +1,10 @@
 function ob(){
-
+    emp = {
+        id : 1, 
+        nama : "John",
+        gaji : 20000
+    }
+    document.write(emp.id + " " + emp.nama + " " + emp.gaji);
 }
 
 function io(){
