080043ec3f273d4d499681b04930511a2ecadc3a
John From Smd <johnfromsmd@gmail.com>
2020-03-22 13:37:35 +0700

    JS Object selesai

--- 2.Advanced/Javascript Object/jsobject.js
+++ 2.Advanced/Javascript Object/jsobject.js
@@ -26,5 +26,19 @@ function oc(){
 }
 
 function mjso(){
-    
+    function emp(id, nama, gaji){
+        this.id = id;
+        this.nama = nama;
+        this.gaji = gaji;
+
+        this.changeGaji = changeGaji;
+
+        function changeGaji(otherGaji){
+            this.gaji = otherGaji;
+        }
+    }
+    e = new emp(4, "John Defeat", 100);
+    document.write(e.id + " " + e.nama + " " + e.gaji);
+    e.changeGaji(10);
+    document.write("<br>" + e.id + " " + e.nama + " " + e.gaji);
 }
