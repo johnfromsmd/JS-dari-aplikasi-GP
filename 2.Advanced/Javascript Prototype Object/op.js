8a7235617171fcfa815cc4af319287ce658a4a7d
John From Smd <johnfromsmd@gmail.com>
2020-03-23 11:42:41 +0700

    Edit JS Prototype Object

--- 2.Advanced/Javascript Prototype Object/op.js
+++ 2.Advanced/Javascript Prototype Object/op.js
@@ -1,19 +1,13 @@
-function op(){
-    function Employee(namaDepan, namaBelakang){
+function op() {
+    function Employee(namaDepan, namaBelakang) {
         this.namaDepan = namaDepan;
         this.namaBelakang = namaBelakang;
     }
-
-    Employee.prototype.namalengkap = function(){
-        return this.namaDepan + " " + namaBelakang;
-    }
-
+    Employee.prototype.namalengkap = function () {
+        return this.namaDepan + " " + this.namaBelakang;
+    };
     var emp1 = new Employee("John", "Haa");
     var emp2 = new Employee("Dohn", "ny");
     document.writeln(emp1.namalengkap() + "<br>");
     document.writeln(emp2.namalengkap());
-}
-
-function op2(){
-
 }
