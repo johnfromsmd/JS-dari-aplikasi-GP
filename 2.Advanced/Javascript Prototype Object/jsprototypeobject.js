325a0e4748a6d6549c9bd041b9b738ca83dfeaa1
John From Smd <johnfromsmd@gmail.com>
2020-03-23 13:22:26 +0700

    finish

--- 2.Advanced/Javascript Prototype Object/jsprototypeobject.js
+++ 2.Advanced/Javascript Prototype Object/jsprototypeobject.js
@@ -5,9 +5,6 @@ function op2(){
     }
 
     Employee.prototype.company = "Google";
-
-    var employee1 = new Employee("John", "John");
-    var employee2 = new Employee("Don", "don");
     document.writeln(employee1.firstName + " " + employee1.lastName + " " + employee1.company + "<br>");
     document.writeln(employee2.firstName + " " + employee2.lastName + " " + employee2.company);
 }
