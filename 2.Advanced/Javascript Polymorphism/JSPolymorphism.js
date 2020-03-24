8c33d512e28dbd3443b5e32d10360d50c71024fd
John From Smd <johnfromsmd@gmail.com>
2020-03-24 14:14:33 +0700

    JS Polymorphism finish

--- 2.Advanced/Javascript Polymorphism/JSPolymorphism.js
+++ 2.Advanced/Javascript Polymorphism/JSPolymorphism.js
@@ -1,65 +1,55 @@
 // f1
-function ibo(){
-    class Moment extends Date {
-        constructor(){
-            super();
+function p(){
+    class A{
+        display(){
+            document.writeln("A is invoked");
         }
     }
-    var m = new Moment();
-    document.writeln("Current date:");
-    document.writeln(m.getDate() + "-" +(m.getMonth()+1) + "-" + m.getFullYear());
+    class B extends A {
+
+    }
+
+    var b = new B();
+        b.display();
 }
 
 // f2
-function ibo2(){
-    class Moment extends Date {
-        constructor(year){
-            super(year);
+function p2(){
+    class A{
+        display(){
+            document.writeln("A is invoked<br>");
         }
     }
-    var m = new Moment("January 1, 1945 20:22:10");
-    document.writeln("Year value:");
-    document.writeln(m.getFullYear());
-}
 
-//f3
-function cc(){
-    class Bike{
-        constructor(){
-            this.company="Honda";
+    class B extends A{
+        display(){
+            document.writeln("B is invoked");
         }
     }
-    class Vechile extends Bike{
-        constructor(name, price){
-            super();
-            this.name=name;
-            this.price=price;
-        }
-    }
-    var v = new Vechile("Ora Beat Oraaaaa", "9000000");
-    document.writeln(v.company+ " "+v.name+ " " + v.price);
+
+    var a = [new A(), new B()]
+    a.forEach(function(msg){
+        msg.display();
+    });
 }
 
-// f4
-function apba(){
-    // constructor function
-    function Bike(company){
-        this.company = company;
+// f3
+function p3(){
+    function A(){
+
     }
 
-    Bike.prototype.getCompany = function(){
-        return this.company;
+    A.prototype.display = function(){
+        return "A is invoked";
     }
 
-    // Another constructor function
-    function Vechile (name, price){
-        this.name = name;
-        this.price = price;
+    function B(){
+        
     }
-    var bike = new Bike("Honda");
-    Vechile.prototype=bike;
-    //Now Bike treats as a parent of Vechile.
-    var vechile = new Vechile("Shine", 700000000);
-    document.writeln(vechile.getCompany()+ " " + vechile.name + " " +vechile.price);
+
+    B.prototype = Object.create(A.prototype);
+    var a = [new A(), new B()]
+    a.forEach(function(msg){
+        document.writeln(msg.display()+ "<br>");
+    });
 }
-
