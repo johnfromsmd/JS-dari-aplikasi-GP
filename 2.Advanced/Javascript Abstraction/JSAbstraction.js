4a66aaa07b087ae744d832e5becf28cb5d50b5b7
John From Smd <johnfromsmd@gmail.com>
2020-03-25 13:30:16 +0700

    JS Abstraction Finish

--- 2.Advanced/Javascript Abstraction/JSAbstraction.js
+++ 2.Advanced/Javascript Abstraction/JSAbstraction.js
@@ -13,6 +13,7 @@ function a(){
     var vechile = new Vechile();
 }
 
+// f2
 function a2(){
     // creating a constructor function
     function Vechile(){
@@ -32,4 +33,21 @@ function a2(){
     Bike.prototype = Object.create(Vechile.prototype);
     var bike = new Bike("Honda");
     document.writeln(bike.display());
+}
+
+// f3
+function a3(){
+    // Creaating constructor function
+    function Vechile(){
+        this.vechileName = vechileName;
+        throw new Error("Kamu tidak membuat sebuah Instance dari Abstract class");
+    }
+    // Creating constructor function
+    function Bike(vechileName){
+        this.vechileName = vechileName;
+    }
+    Bike.prototype = Object.create(Vechile.prototype);
+    var bike = new Bike("Honda");
+    document.writeln(bike instanceof Vechile);
+    document.writeln(bike instanceof Bike);
 }
